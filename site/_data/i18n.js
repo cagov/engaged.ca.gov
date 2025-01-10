b23d7ddf4f82fb79fdd4a3703efa403372ed15b7
Jim Bumgardner <jbum@jbum.com>
2025-01-10 09:53:52 -0800

    Extracted several text strings.

--- site/_data/i18n.js
+++ site/_data/i18n.js
@@ -25,10 +25,135 @@ const translations = {
     'es': 'Regístrate ahora',
     location: "Home",
   },
-  'tell-your-government': {
-    'en': 'Tell your government how you feel about pressing issues',
-    'es': 'Habla con tu gobierno sobre los temas más importantes',
+  'official-website-prefix': {
+    'en': 'Official website of the',
+    'es': 'Sitio web oficial del',
+    location: "Site header",
+  },
+  'state-of-california': {
+    'en': 'State of California',
+    'es': 'Estado de California',
+    location: "Site header",
+  },
+  'how-it-works': {
+    'en': 'How it works',
+    'es': 'Cómo funciona',
     location: "Home",
+  },
+  'how-it-works-step-1': {
+    'en': 'You opt in to a topic',
+    'es': 'Optas por un tema',
+    location: "Home",
+  },
+  'how-it-works-step-2': {
+    'en': 'We invite you to join',
+    'es': 'Te invitamos a unirte',
+    location: "Home",
+  },
+  'how-it-works-step-3': {
+    'en': 'You share your thoughts',
+    'es': 'Compartes tus ideas',
+    location: "Home",
+  },
+  'how-it-works-step-4': {
+    'en': 'We share results',
+    'es': 'Compartimos los resultados',
+    location: "Home",
+  },
+  'join-the-conversation': {
+    'en': 'Join the conversation',
+    'es': 'Únete a la conversación',
+    location: "Home",
+  },
+  'join-the-conversation-form-label': {
+    'en': 'Give us your email. We\'ll notify you when topics open.',
+    'es': 'Dinos tu correo electrónico. Te notificaremos cuando los temas estén disponibles.',
+    location: "Home",
+  },
+  'back-to-top': {
+    'en': 'Back to top',
+    'es': 'Volver arriba',
+    location: "Footer",
+  },
+  'email-label': {
+    'en': 'Email',
+    'es': 'Correo electrónico',
+    location: "Home",
+  },
+  'required-label': {
+    'en': 'Required field',
+    'es': 'Campo requerido',
+    location: "Home",
+  },
+  'valid-email-message': {
+    'en': 'Please enter a valid email address.',
+    'es': 'Por favor, ingresa una dirección de correo electrónico válida.',
+    location: "Home",
+  },
+  'valid-email-message-2': {
+    'en': 'Email addresses should contain a "@" and a "."',
+    'es': 'Las direcciones de correo electrónico deben contener un "@" y un "."',
+    location: "Home",
+  },
+  'form-problem-message': {
+    'en': 'We\'re sorry, there\'s a problem with our form.',
+    'es': 'Lo sentimos, hay un problema con nuestro formulario.',
+    location: "Home",
+  },
+  'form-problem-message-2': {
+    'en': 'Please try again later.',
+    'es': 'Por favor, inténtalo de nuevo más tarde.',
+    location: "Home",
+  },
+  'form-submit-button': {
+    'en': 'Sign up now',
+    'es': 'Regístrate ahora',
+    location: "Home",
+  },
+  'form-success-message': {
+    'en': 'Your subscription is confirmed.',
+    'es': 'Tu suscripción está confirmada.',
+    location: "Home",
+  },
+  'form-success-message-2': {
+    'en': 'Thank you for signing up! Look out for news and updates.',
+    'es': '¡Gracias por registrarte! Mantente al tanto de las noticias y actualizaciones.',
+    location: "Home",
+  },
+  'privacy-policy': {
+    'en': 'Privacy policy',
+    'es': 'Política de privacidad',
+    location: "Footer",
+  },
+  'website-accessibility-certification': {
+    'en': 'Website accessibility certification',
+    'es': 'Certificación de accesibilidad del sitio web',
+    location: "Footer",
+  },
+  'form-privacy-message': {
+    'en': 'We take your <a href="/privacy">privacy</a> seriously.',
+    'es': 'Nos tomamos tu <a href="/es/privacy">privacidad</a> muy en serio.',
+    location: "Home",
+  },
+  'form-privacy-message-2': {
+    'en': 'You can unsubscribe at any time.',
+    'es': 'Puedes darte de baja en cualquier momento.',
+    location: "Home",
+  },
+  'about-this-program': {
+    'en': 'About this program',
+    'es': 'Sobre este programa',
+    location: "Footer",
+  },
+  'conditions-of-use': {
+    'en': 'Conditions of use',
+    'es': 'Condiciones de uso',
+    location: "Footer",
+  },
+  'sitemap': {
+    'en': 'Sitemap',
+    'es': 'Mapa del sitio',
+    location: "Footer",
   },
 };
 // add default export
