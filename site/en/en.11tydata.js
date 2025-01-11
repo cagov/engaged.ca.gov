c993434451e65ce0d743b42200d4d4e570d86cea
Jon Jensen <github@id.xjensen.com>
2025-01-10 19:08:25 -0800

    Fix absent homepage

--- site/en/en.11tydata.js
+++ site/en/en.11tydata.js
@@ -2,6 +2,6 @@ export default {
   eleventyComputed: {
     // Render the English pages at root of the site, without /en/ prefixing the path.
     permalink: (item) =>
-      item.permalink === false ? false : `/${item.page.filePathStem.replace("/en/", "/")}/`,
+      item.permalink === false || item.permalink === "/" ? item.permalink : `/${item.page.filePathStem.replace("/en/", "/")}/`,
   },
 };
