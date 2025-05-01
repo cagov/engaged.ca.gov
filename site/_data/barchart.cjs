66aa86c048c2561cf30b5d3ac871d46266953f98
Jim Bumgardner <jbum@jbum.com>
2025-04-30 18:28:43 -0700

    Added barchart hide/show.

--- site/_data/barchart.cjs
+++ site/_data/barchart.cjs
@@ -1,10 +1,17 @@
 // Import the barchart data
-const fs = require('fs');
-const path = require('path');
 
 // Read the JSON file
-const barchartDataPath = path.join(__dirname, './barchart.json');
-const barchartData = JSON.parse(fs.readFileSync(barchartDataPath, 'utf8'));
-
+const barchartData = [
+    {"key":"caption1", "caption": "Debris removal & environmental recovery", "approval":85.27},
+    {"key":"caption2", "caption": "Wildfire prevention prioritization & accountability", "approval":84.31},
+    {"key":"caption3", "caption": "Climate & community resilience", "approval":83.21},
+    {"key":"caption4", "caption": "Infrastructure & utilities restoration", "approval":82.93},
+    {"key":"caption5", "caption": "Emergency planning & community safety", "approval":81.88},
+    {"key":"caption6", "caption": "Housing & rebuilding", "approval":79.14},
+    {"key":"caption7", "caption": "Financial & legal assistance", "approval":77.54},
+    {"key":"caption8", "caption": "Emergency communication", "approval":76.46},
+    {"key":"caption9", "caption": "Economic recovery & small business support", "approval":72.40},
+    {"key":"caption10", "caption": "Emotional & mental health support", "approval":48.77}
+   ]
 // Export the data for use in templates
 module.exports = barchartData; 
