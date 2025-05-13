8778c44ab542af3246aef2886750d00a9a3f2d47
Zakiya <915352+zakiya@users.noreply.github.com>
2025-05-13 13:01:45 -0700

    E3 page - Keeping separate copy of new strings.

---
+++ site/_data/i18n-government-efficiencies.js
@@ -0,0 +1,25 @@
+import translations from "./i18n.js";
+
+const translationsGovernmentEfficiencies = {
+  "topic-government-efficiencies": {
+    en: "I certify I am a current State of California employee",
+    fr: "I certify I am a current State of California employee",
+    es: "I certify I am a current State of California employee",
+    ko: "I certify I am a current State of California employee",
+    vi: "I certify I am a current State of California employee",
+    tl: "I certify I am a current State of California employee",
+    "zh-hans": "I certify I am a current State of California employee",
+    "zh-hant": "I certify I am a current State of California employee",
+    fa: "I certify I am a current State of California employee",
+    hy: "I certify I am a current State of California employee",
+    location: "Government efficiencies page",
+  },
+};
+
+const AllTranslations = Object.assign(
+  {},
+  translationsGovernmentEfficiencies,
+  translations,
+);
+
+export default AllTranslations;
