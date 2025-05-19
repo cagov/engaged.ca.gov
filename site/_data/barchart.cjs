80e8431036719fa1fe9b1511c1a387f36d350ffd
Jim Bumgardner <jbum@jbum.com>
2025-05-19 15:41:33 -0700

    Content update.

--- site/_data/barchart.cjs
+++ site/_data/barchart.cjs
@@ -2,16 +2,16 @@
 
 // Read the JSON file
 const barchartData = [
-    {"key":"caption1", "caption": "Debris removal & environmental recovery", "approval":85.27},
-    {"key":"caption2", "caption": "Wildfire prevention prioritization & accountability", "approval":84.31},
-    {"key":"caption3", "caption": "Climate & community resilience", "approval":83.21},
-    {"key":"caption4", "caption": "Infrastructure & utilities restoration", "approval":82.93},
-    {"key":"caption5", "caption": "Emergency planning & community safety", "approval":81.88},
-    {"key":"caption6", "caption": "Housing & rebuilding", "approval":79.14},
-    {"key":"caption7", "caption": "Financial & legal assistance", "approval":77.54},
-    {"key":"caption8", "caption": "Emergency communication", "approval":76.46},
-    {"key":"caption9", "caption": "Economic recovery & small business support", "approval":72.40},
-    {"key":"caption10", "caption": "Emotional & mental health support", "approval":48.77}
+    {"key":"caption1", "caption": "Environmental recovery and clean-up", "approval":87.34},
+    {"key":"caption2", "caption": "Wildfire prevention prioritization & accountability", "approval":86.45},
+    {"key":"caption3", "caption": "Climate and community resilience", "approval":85.33},
+    {"key":"caption4", "caption": "Infrastructure and utilities restoration", "approval":85.35},
+    {"key":"caption5", "caption": "Emergency planning and community safety", "approval":83.28},
+    {"key":"caption6", "caption": "Housing and rebuilding", "approval":81.87},
+    {"key":"caption7", "caption": "Financial and legal assistance", "approval":79.12},
+    {"key":"caption8", "caption": "Emergency communication", "approval":77.25},
+    {"key":"caption9", "caption": "Economic recovery and small business support", "approval":73.51},
+    {"key":"caption10", "caption": "Emotional and mental health support", "approval":50.43}
    ]
 // Export the data for use in templates
 module.exports = barchartData; 
