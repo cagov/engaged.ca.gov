6a3000465de1d09f44154feb1ce90a7597eb98f7
Jim Bumgardner <jbum@jbum.com>
2025-05-29 12:30:43 -0700

    Restyled bar charts for desktop and mobile.

--- site/_data/barchart.cjs
+++ site/_data/barchart.cjs
@@ -2,16 +2,16 @@
 
 // Read the JSON file
 const barchartData = [
-    {"key":"caption1", "caption": "Environmental recovery and clean-up", "approval":87.34},
-    {"key":"caption2", "caption": "Wildfire prevention prioritization & accountability", "approval":86.45},
-    {"key":"caption3", "caption": "Climate and community resilience", "approval":85.33},
-    {"key":"caption4", "caption": "Infrastructure and utilities restoration", "approval":85.35},
-    {"key":"caption5", "caption": "Emergency planning and community safety", "approval":83.28},
-    {"key":"caption6", "caption": "Housing and rebuilding", "approval":81.87},
-    {"key":"caption7", "caption": "Financial and legal assistance", "approval":79.12},
-    {"key":"caption8", "caption": "Emergency communication", "approval":77.25},
-    {"key":"caption9", "caption": "Economic recovery and small business support", "approval":73.51},
-    {"key":"caption10", "caption": "Emotional and mental health support", "approval":50.43}
+    {"key":"caption1", "caption": "Environmental recovery and clean-up", "approval":87.34,"link_id":"findings_environmental_recovery"},
+    {"key":"caption2", "caption": "Wildfire prevention prioritization & accountability", "approval":86.45,"link_id":"findings_wildfire_prevention"},
+    {"key":"caption3", "caption": "Climate and community resilience", "approval":85.33,"link_id":"findings_climate_community_resilience"},
+    {"key":"caption4", "caption": "Infrastructure and utilities restoration", "approval":85.35,"link_id":"findings_infrastructure_restoration"},
+    {"key":"caption5", "caption": "Emergency planning and community safety", "approval":83.28,"link_id":"findings_emergency_planning_safety"},
+    {"key":"caption6", "caption": "Housing and rebuilding", "approval":81.87,"link_id":"findings_housing_rebuilding"},
+    {"key":"caption7", "caption": "Financial and legal assistance", "approval":79.12,"link_id":"findings_financial_legal_assistance"},
+    {"key":"caption8", "caption": "Emergency communication", "approval":77.25,"link_id":"findings_emergency_communication"},
+    {"key":"caption9", "caption": "Economic recovery and small business support", "approval":73.51,"link_id":"findings_economic_recovery"},
+    {"key":"caption10", "caption": "Emotional and mental health support", "approval":50.43,"link_id":"findings_emotional_mental_health"}
    ]
 // Export the data for use in templates
 module.exports = barchartData; 
