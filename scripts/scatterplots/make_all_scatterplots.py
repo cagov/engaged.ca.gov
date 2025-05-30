f13ee1f722468858708945dfc75ea6fc0072f669
Jim Bumgardner <jbum@jbum.com>
2025-05-30 16:07:22 -0700

    Updated three charts.

--- scripts/scatterplots/make_all_scatterplots.py
+++ scripts/scatterplots/make_all_scatterplots.py
@@ -1,42 +1,50 @@
 # make scatterplots
 import subprocess
-
+import os
 languages = ['en', 'es', 'fa', 'hy', 'ko', 'tl','vi', 'zh-hans', 'zh-hant']
 
-theme_recs = [{'theme':'Public v Private', 'filename_root':'public_private'},
-              {'theme':'Individual v. Collective Mitigation', 'filename_root':'ind_vs_collective'},
-              # {'theme':'Financial Aid Distribution', 'filename_root':'financial_aid'},
-              {'theme':'Build Faster v. Build Better', 'filename_root':'build_faster_vs_better'},
-              {'theme':'Density v. Safety', 'filename_root':'density_vs_safety'}]
+theme_recs = [{'theme':'Environmental recovery and clean-up','root':'environmental_recovery'}, 
+       {'theme':'Wildfire prevention prioritization and accountability','root':'wildfire_prevention'}, 
+       {'theme':'Climate and community resilience','root':'climate_community_resilience'}, 
+       {'theme':'Infrastructure & utilities restoration','root':'infrastructure_restoration'}, 
+       {'theme':'Emergency communication','root':'emergency_planning_safety'}, 
+       {'theme':'Housing and rebuilding','root':'housing_rebuilding'}, 
+       {'theme':'Financial and legal assistance','root':'financial_legal_assistance'}, 
+       {'theme':'Emergency communication','root':'emergency_communication'}, 
+       {'theme':'Economic recovery','root':'economic_recovery'}, 
+       {'theme':'Emotional and mental health','root':'emotional_mental_health'}]
+#  python3 make_scatterplot.py housing_rebuilding_v4.json "Housing and rebuilding" -out plots/housing_rebuilding.svg
 
 out_dir = './plots'
 
 src_file = 'engca_comment_scatterplot_source_V3.json'
-
+version = 'v4'
 for theme_rec in theme_recs:
     theme = theme_rec['theme']
-    filename_root = theme_rec['filename_root']
-    # output just the dots here...
-    # cmd = f'python3 make_scatterplot.py engca_comment_scatterplot_source_V2.json "{theme}" -out {out_dir}/{filename_root}_plot.svg'
-    # print(cmd)
-    # subprocess.run(cmd, shell=True)
+    filename_root = theme_rec['root']
+    src_file_csv = f'./data_{filename_root}_{version}.csv'
+    src_file_json = f'./data_{filename_root}_{version}.json'
+    if not os.path.exists(src_file_csv):
+        continue
+    if not os.path.exists(src_file_json):
+        cmd = f'python3 convert_csv_to_json.py {src_file_csv} {src_file_json}'
+        print(cmd)
+        subprocess.run(cmd, shell=True)
     for language in languages:
         print(f"Making {theme} legend in {language}")
         # make scatterplot
-        cmd = f'python3 make_scatterplot.py {src_file} "{theme}" -legend -lang {language} -out {out_dir}/{filename_root}_{language}_legend.svg'
+        cmd = f'python3 make_scatterplot.py {src_file_json} "{theme}" -legend -lang {language} -out {out_dir}/findings_{filename_root}_{language}_legend.svg'
         print(cmd)
         subprocess.run(cmd, shell=True)
 
 for theme_rec in theme_recs:
     theme = theme_rec['theme']
-    filename_root = theme_rec['filename_root']
+    filename_root = theme_rec['root']
+    src_file_json = f'./data_{filename_root}_{version}.json'
+    if not os.path.exists(src_file_json):
+        continue
     # output just the dots here...
-    cmd = f'python3 make_scatterplot.py {src_file} "{theme}" -out {out_dir}/{filename_root}_plot.svg'
+    cmd = f'python3 make_scatterplot.py {src_file_json} "{theme}" -out {out_dir}/findings_{filename_root}.svg'
     print(cmd)
     subprocess.run(cmd, shell=True)
 
-# python3 make_scatterplot.py engca_comment_scatterplot_source_V2.json "Public v Private" plots/public_private.svg
-# python3 make_scatterplot.py engca_comment_scatterplot_source_V2.json "Individual v. Collective Mitigation" plots/ind_vs_collective.svg
-# python3 make_scatterplot.py engca_comment_scatterplot_source_V2.json "Financial Aid Distribution" plots/financial_aid.svg
-# python3 make_scatterplot.py engca_comment_scatterplot_source_V2.json "Build Faster v. Build Better" plots/build_faster_vs_better.svg
-# python3 make_scatterplot.py engca_comment_scatterplot_source_V2.json "Density v. Safety" plots/density_vs_safety.svg
