6e34303a3ed6225d5b68b9ea52692dd403ea7cc8
Zakiya <915352+zakiya@users.noreply.github.com>
2025-07-16 17:09:32 -0700

    Add light logging.

--- scripts/google-apps/engca_comment_import_script.sh
+++ scripts/google-apps/engca_comment_import_script.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 open -a "Safari" https://odi.ethelo.net/admin/decisions/33/comment_moderations/csv_download
 sleep 1m
 cd ~/Google\ Drive/My\ Drive/Comments/
+
+LOG_FILE="$HOME/cron_job.log"
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
 mv comments-2.csv comments.csv
+echo "[$DATE] EngCA comment import happened" >> "$LOG_FILE"
