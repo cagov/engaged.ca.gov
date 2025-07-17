a138c08459a3e672de69135e3976e08a123878e5
Zakiya <915352+zakiya@users.noreply.github.com>
2025-07-17 09:51:36 -0700

    Remove previous file before downloading the new one

--- scripts/google-apps/engca_comment_import_script.sh
+++ scripts/google-apps/engca_comment_import_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+rm ~/Google\ Drive/My\ Drive/Comments/comments-2.csv
 open -a "Safari" https://odi.ethelo.net/admin/decisions/33/comment_moderations/csv_download
 sleep 1m
 cd ~/Google\ Drive/My\ Drive/Comments/
 
 LOG_FILE="$HOME/cron_job.log"
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
 mv comments-2.csv comments.csv
 echo "[$DATE] EngCA comment import happened" >> "$LOG_FILE"
