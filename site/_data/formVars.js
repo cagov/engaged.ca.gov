967d3444aa0230e571613fed2b9c806a681d4838
Zakiya <915352+zakiya@users.noreply.github.com>
2025-08-08 19:26:59 -0700

    Get all form elements on the page

---
+++ site/_data/formVars.js
@@ -0,0 +1,27 @@
+const vars = {
+  label_fires: "LA Fires Recovery",
+  label_employees: "State Employees",
+  label_future: "Future topics",
+  label_employees_question: "Are you a current State of California employee?",
+  label_yes: "Yes",
+  label_no: "No",
+  mailchimp_config: [
+    {
+      audience_name: "Engaged California",
+      audience_id: "61200a6dda",
+      options: 1,
+    },
+    {
+      audience_name: "EngCA - State Employees",
+      audience_id: "417d16b2c0",
+      options: 2,
+    },
+    {
+      audience_name: "[SANDBOX] EngCA testing",
+      audience_id: "23461fc80f",
+      options: 2,
+    },
+  ],
+};
+
+export default vars;
