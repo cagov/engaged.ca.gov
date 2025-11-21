5928bb9b01b1989c6799903c0e670222772a406d
Jim Bumgardner <jbum@jbum.com>
2025-11-21 09:04:02 -0800

    Backed up redirect scripts. Reduced width of column for titles.

---
+++ scripts/pr_redirect_script_1.0.1.js
@@ -0,0 +1,38 @@
+// 1.0.1 jbum - disabling authentication for e3-report - DOES NOT WORK
+// 1.0 Jon Jensen
+
+function handler(event) {
+    const request = event.request;
+    const uri_lc = request.uri.toLowerCase();
+    const headers = request.headers;
+    const authHeaders = headers.authorization;
+    
+    const credentials = "odiandfriends:odiandfriends"; /* username:password */
+    const authHeaderStr = "Basic " + Buffer.from(credentials).toString('base64');
+    
+    // If credentials are good, perform PR redirect.
+    if ((authHeaders && authHeaders.value == authHeaderStr) || uri_lc.includes("e3-report")) { 
+        const host = headers.host.value;
+        const domain = "pr.engaged.ca.gov";
+        const subdomain = host.substr(0, host.indexOf(`.${domain}`));
+        
+        // Rewrite request path to branch-based pr subfolder.
+        if (subdomain && !request.uri.startsWith("/pr/")) {
+            request.uri = `/pr/${subdomain}${request.uri}`;
+        }
+        
+        return request;
+    }
+
+    
+    // If credentials are bad, respond with 401 and request credentials.
+    const response = {
+        statusCode: 401,
+        statusDescription: 'Unauthorized',
+        headers: { 
+            "www-authenticate": { value: 'Basic realm="Authentication"' } 
+        }
+    };
+
+    return response;
+}
